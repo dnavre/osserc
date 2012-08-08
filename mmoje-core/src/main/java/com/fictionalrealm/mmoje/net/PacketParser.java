fdea900230481188fc6f5ba15e8eb5b916f9f53e
Yervand Aghababyan <yervand.aghababyan@gmail.com>
2012-08-09 02:17:01 +0400

    minor additions to packet processing

---
+++ mmoje-core/src/main/java/com/fictionalrealm/mmoje/net/PacketParser.java
@@ -0,0 +1,11 @@
+package com.fictionalrealm.mmoje.net;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yervand.Aghababyan
+ * Date: 8/9/12
+ * Time: 2:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PacketParser {
+}
